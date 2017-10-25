f5512849e4b2a709ed7e348579929cb9d169853a
Willian de Souza <willian.souza@chaordicsystems.com>
2017-10-25 21:35:46 -0200

    Removido comentarios e criado uma função para as mascaras

--- js/controllerGUI.js
+++ js/controllerGUI.js
@@ -4,96 +4,77 @@
 
 $(document).ready(function(){
 
-
-// $('.input-tce').hide();
-    // $('.input-ts').hide();
-    // $('.input-falha').hide();
-    // $('.input-tfalha').hide();
-
     $( '#botoes-controle').hide();
-
     $('#img-loader').hide();
 
+    // Define valores iniciais para os campos das funçoes geradoras
+    // Facilita testar :D
     $('#valor-tce').val('uniforme');
     $('#input-tce').val('6,9');
-    $('#duracao-simulacao').val('1');
-    // $('#valor-tce').change(function(){
-    //     var tipoTCE = this.value;
-    //     if(tipoTCE != "default"){
-    //         var atributos =  getAtributosInput(tipoTCE);
-    //         $("#input-tce").attr("placeholder", atributos[0]);
-    //         $("#input-tce").mask(atributos[1], {
-    //             translation: {
-    //                 'Z': {
-    //                     pattern: /[0-9]/, optional: true
-    //                 }
-    //             }
-    //         });
-    //         $('.input-tce').show();
-    //     } else {
-    //         $('.input-tce').hide();
-    //     }
-    // });
 
     $('#valor-ts').val('uniforme');
     $('#input-ts').val('8,13');
-    // $('#valor-ts').change(function(){
-    //     var tipoTS = this.value;
-    //     if(tipoTS != "default"){
-    //         var atributos =  getAtributosInput(tipoTS);
-    //         $("#input-ts").attr("placeholder", atributos[0]);
-    //         $("#input-ts").mask(atributos[1], {
-    //             translation: {
-    //                 'Z': {
-    //                     pattern: /[0-9]/, optional: true
-    //                 }
-    //             }
-    //         });
-    //         $('.input-ts').show();
-    //     } else {
-    //         $('.input-ts').hide();
-    //     }
-    // });
 
     $('#tempo-falha').val('uniforme');
     $('#input-tfalha').val('45, 60');
-    // $('#tempo-falha').change(function(){
-    //     var tipoTFalha = this.value;
-    //     if(tipoTFalha != "default"){
-    //         var atributos =  getAtributosInput(tipoTFalha);
-    //         $("#input-tfalha").attr("placeholder", atributos[0]);
-    //         $("#input-tfalha").mask(atributos[1], {
-    //             translation: {
-    //                 'Z': {
-    //                     pattern: /[0-9]/, optional: true
-    //                 }
-    //             }
-    //         });
-    //         $('.input-tfalha').show();
-    //     } else {
-    //         $('.input-tfalha').hide();
-    //     }
-    // });
 
     $('#valor-falha').val('constante');
     $('#input-falha').val('15');
-    // $('#valor-falha').change(function(){
-    //     var tipoFalha = this.value;
-    //     if(tipoFalha != "default"){
-    //         var atributos =  getAtributosInput(tipoFalha);
-    //         $("#input-falha").attr("placeholder",atributos[0]);
-    //         $("#input-falha").mask(atributos[1  ], {
-    //             translation: {
-    //                 'Z': {
-    //                     pattern: /[0-9]/, optional: true
-    //                 }
-    //             }
-    //         });
-    //         $('.input-falha').show();
-    //     } else {
-    //         $('.input-falha').hide();
-    //     }
-    // });
+
+    $('#duracao-simulacao').val('1');
+    ///////////////////////////////////////////////////////////
+
+
+    $('#valor-tce').change(function() {
+        atualizarCampoModificado(this.value, 'input-tce');
+    });
+
+    $('#valor-ts').change(function() {
+        atualizarCampoModificado(this.value, 'input-ts');
+    });
+
+    $('#tempo-falha').change(function() {
+        atualizarCampoModificado(this.value, 'input-tfalha');
+    });
+
+    $('#valor-falha').change(function() {
+        atualizarCampoModificado(this.value, 'input-falha');
+    });
+
+    $('#duracao-simulacao').mask("ZZZ", {
+                translation: {
+                    'Z': {
+                        pattern: /[0-9]/, optional: true
+                    }
+                }
+    });
+
+    $('#start').click(function(){
+        $('#start').hide();
+        $( '#botoes-controle').show();
+    });
+
+    $('#reload').click(function(){
+        location.reload();
+    });
+
+
+    function atualizarCampoModificado(valorDoCampoOption, nomeDoInput) {
+        if(valorDoCampoOption !== "default"){
+            var atributos =  getAtributosInput(valorDoCampoOption);
+            $(`#${nomeDoInput}`).attr("placeholder", atributos[0]);
+            $(`#${nomeDoInput}`).mask(atributos[1], {
+                translation: {
+                    'Z': {
+                        pattern: /[0-9]/, optional: true
+                    }
+                }
+            });
+            $(`.${nomeDoInput}`).show();
+        } else {
+            $(`.${nomeDoInput}`).hide();
+        }
+    }
 
     function getAtributosInput(valor){
 
@@ -117,21 +98,4 @@ $(document).ready(function(){
                 return "erro";
         }
     }
-
-    $('#duracao-simulacao').mask("ZZZ", {
-                translation: {
-                    'Z': {
-                        pattern: /[0-9]/, optional: true
-                    }
-                }
-    });
-
-    $('#start').click(function(){
-        $('#start').hide();
-        $( '#botoes-controle').show();
-    });
-
-    $('#reload').click(function(){
-        location.reload();
-    });
 });
